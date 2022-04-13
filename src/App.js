71100d853e9c4e2fa3f4bb6bed5d10b7195b11b5
Lucas <lucas2carlos@gmail.com>
2022-04-13 16:22:44 -0400

    building App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+import "./style.css"
+import { useState } from "react"
 
 export default function App(){
+    const [input, setInput] = useState('')
     return(
         <div className="App">
-            <h1>Hello World!</h1>
+            <h1>My To Do List:</h1>
+            <div id="input-field">
+                <small>New Item</small>
+                <br />
+                <input
+                type="text"
+                value={input}
+                onChange={(e) => {
+                    setInput(e.target.value)
+                }}
+                />
+            </div>
+            <br/>
+            <div>
+                <h2>To do Items: </h2>
+                <ul>
+                    <li> Learn more about React</li>
+                    <li> Write a new Component</li>
+                    <li>Add some style</li>
+                </ul>
+            </div>
         </div>
     )
 }
