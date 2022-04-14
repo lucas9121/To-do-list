f5e8abcfa728e7dce8dbd3067121595f21264a28
Lucas <lucas2carlos@gmail.com>
2022-04-13 23:09:12 -0400

    building App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import "./style.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import toDoListData from "./data"
 
 export default function App(){
-    const [input, setInput] = useState('')
+    const [toDo, setToDo] = useState(toDoListData)
+    const [newData, setNewData] = useState({
+        title: '',
+        completed: false
+    })
+
+    const handleChange = (event) => {
+        // adding [] around the event.target.name makes the name variable in the target event the key of the object that is being made. So in this case the name is title because of the input name
+        setNewData({[event.target.name]: event.target.value, completed: false})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setToDo([...toDoListData, newData] )
+    }
+
+    // const addData = () => {
+    //     setNewData(input)
+    //     toDoListData.push({title: newData, completed: false})
+    // }
+
+    useEffect(() => {
+        console.log(toDoListData)
+        console.log(toDo)
+        // console.log(input)
+    }, [toDo])
+
     return(
         <div className="App">
             <h1>My To Do List:</h1>
             <div id="input-field">
                 <small>New Item</small>
                 <br />
-                <input
-                type="text"
-                value={input}
-                onChange={(e) => {
-                    setInput(e.target.value)
-                }}
-                />
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type="text"
+                    name="title"
+                    value={newData.title}
+                    onChange={handleChange}
+                    />
+                    <button type="submit">submit</button>
+                </form>
             </div>
             <br/>
             <div>
                 <h2>To do Items: </h2>
                 <ul>
-                    <li> Learn more about React</li>
-                    <li> Write a new Component</li>
-                    <li>Add some style</li>
+                    {toDoListData.map((toDo, idx) => {
+                        return(
+                            <li key={idx}>{toDo.title}</li>
+                        )
+                    })}
+                    {toDo.forEach((todo, i) => {
+                        console.log(todo.title)
+                        return(
+                            <li key={i}>{todo.title} Hello</li>
+                        )
+                    })}
                 </ul>
             </div>
         </div>
