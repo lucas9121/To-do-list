ba3d85754a6567647bc2c98756629a1a8a04408a
Lucas <lucas2carlos@gmail.com>
2022-04-17 21:05:05 -0400

    building Input

---
+++ src/components/Input.js
@@ -0,0 +1,22 @@
+export default function Input ({handleSubmit, setNewData}) {
+    const handleChange = (event) => {
+        // adding [] around the event.target.name makes the name variable in the target event the key of the object that is being made. So in this case the name is title because of the input name
+        setNewData({[event.target.name]: event.target.value, completed: false})
+    }
+    return (
+        <main>
+            <small>New Item</small>
+                <br />
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type="text"
+                    name="title"
+                    id="todo"
+                    value={newData.title}
+                    onChange={handleChange}
+                    />
+                    <button type="submit" style={{display: 'none'}} >submit</button>
+                </form>
+        </main>
+    )
+}
