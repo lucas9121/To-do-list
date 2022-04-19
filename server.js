46a40f27f29060b060cc2cb51f76934a5d486c3d
Lucas <lucas2carlos@gmail.com>
2022-04-19 19:19:05 -0400

    building server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const taskController = require('./controllers/task')
 
 
