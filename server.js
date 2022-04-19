07240393b8740c65df3f4bd02bc7c257db06a593
Lucas <lucas2carlos@gmail.com>
2022-04-19 19:22:33 -0400

    building server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path');
+const favicon = require('serve-favicon');
 const PORT = process.env.PORT || 3001
 const taskController = require('./controllers/task')
 
@@ -9,6 +11,11 @@ const taskController = require('./controllers/task')
 app.use(cors())
 app.use(express.json())
 app.use('/tasks', taskController)
+
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.listen(PORT, () => {
