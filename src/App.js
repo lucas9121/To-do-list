ec660f12fe436e3e2a5a32c11bdba540add43724
Lucas <lucas2carlos@gmail.com>
2022-04-19 19:45:53 -0400

    building App

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App(){
     useEffect(() => {
         (async () => {
             try {
-                const response = await fetch(`http://localhost:3000/`)
+                const response = await fetch(`http://localhost:3000/tasks`)
             } catch(e) {
                 console.log(e)
             }
