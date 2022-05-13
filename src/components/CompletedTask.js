9ad6f467444ce67ac97e552ba4aa25941340b291
Lucas <lucas2carlos@gmail.com>
2022-05-13 00:32:44 -0400

    adding completed tasks

---
+++ src/components/CompletedTask.js
@@ -0,0 +1,25 @@
+import { handleClick } from "../utilities/task-api"
+export default function Completed({tasks}) {
+    return (
+        <div>
+            <h2>Completed:</h2>
+            <ul>
+                {tasks.map((task, idx) => {
+                    return(
+                        task.status === true ?
+                        <li key={idx}>{task.title} <button onClick={() => {handleClick(false, task._id)}}>To do</button> <button onClick={() => {
+                            try {
+                                fetch(`/tasks/${task._id}`, {method: 'DELETE'})
+                            } catch(e) {
+                                console.log(e)
+                            } finally {
+                                setBtnPresed(!btnPressed)
+                            }
+                        }}>Remove</button></li>:
+                        null
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
