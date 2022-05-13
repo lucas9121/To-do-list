23ae01a01ebf7e5fa2c5156c690749b609b21475
Lucas <lucas2carlos@gmail.com>
2022-05-13 00:22:23 -0400

    fixing route

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import { React, StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./style.css"
 import App from "./App"
-import Layout from "./Layout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
     <StrictMode>
         <Router>
             <Routes>
-                <Route path="/" element={ <Layout /> }>
-                    <Route index element ={ <App />} />
-                </Route>
+                <Route path="/" element={ <App /> } />
             </Routes>
         </Router>
     </StrictMode>,
