ca8409fbdea2988b63a3d3bfc479be5d5c17d977
Lucas <lucas2carlos@gmail.com>
2022-05-13 00:59:08 -0400

    adding favicon code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 const path = require('path');
 require('./models/connection')
-// const favicon = require('serve-favicon')
+const favicon = require('serve-favicon')
 const taskController = require('./controllers/task')
 
 
@@ -14,7 +14,7 @@ app.use(express.json())
 
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
-// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/tasks', taskController)
