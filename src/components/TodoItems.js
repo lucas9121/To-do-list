b6a939acb52bb25f0ca3357392966cea9d8c323b
Lucas <lucas2carlos@gmail.com>
2022-05-13 00:29:53 -0400

    adding todo tasks

---
+++ src/components/TodoItems.js
@@ -0,0 +1,17 @@
+import { handleClick } from "../utilities/task-api"
+export default function Todo() {
+    return (
+        <div>
+            <h2>To do Items: </h2>
+            <ul>
+                {tasks.map((task, i) => {
+                    return(
+                        task.status === false ?
+                        <li key={i}>{task.title} <button onClick={() => {handleClick(true, task._id)}} >Completed</button> </li> : 
+                        null
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
